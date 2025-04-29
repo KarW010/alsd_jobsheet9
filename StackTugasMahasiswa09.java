85d006cc5b0509f4e17e6e1b7714f740730f7635
KarW010 <kartikostudy@gmail.com>
2025-04-29 16:03:18 +0700

    Jobsheet9 2.2

--- StackTugasMahasiswa09.java
+++ StackTugasMahasiswa09.java
@@ -69,7 +69,7 @@ public class StackTugasMahasiswa09 {
 
     public String konversiDesimalKeBiner(int nilai) {
         StackKonversi09 stack = new StackKonversi09();
-        while (nilai > 0) {
+        while (nilai != 0) {
             int sisa = nilai % 2;
             stack.push(sisa);
             nilai = nilai / 2;
